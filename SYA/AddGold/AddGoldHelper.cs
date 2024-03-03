e676f851b7106c83dbd65d4ae42e5061d298de7e
PRATYUSH-RAJPARA <PVRAJPARA.CE@GMAIL.COM>
2024-03-04 00:10:20 +0530

    Working on AddGold Pending

---
+++ SYA/AddGold/AddGoldHelper.cs
@@ -0,0 +1,128 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SYA.AddGold
+{
+    public class AddGoldHelper
+    {
+        public static void PrintPage(object sender, PrintPageEventArgs e, DataGridViewRow selectedRow)
+        {
+            if (selectedRow != null)
+            {
+                string tagNumber = (selectedRow.Cells["tagno"].Value ?? "0").ToString();
+                if (tagNumber.Length > 1)
+                {
+
+                    //  Font font = new Font("Arial Black", 8, FontStyle.Bold); // Adjust the font size
+                    SolidBrush brush = new SolidBrush(Color.Black);
+
+
+                    if ((selectedRow.Cells["gross"].Value ?? "0").ToString() == (selectedRow.Cells["net"].Value ?? "0").ToString())
+                    {
+                        e.Graphics.DrawString("G: " + (selectedRow.Cells["gross"].Value ?? "0").ToString(), new Font("Arial", (float)12, FontStyle.Bold), brush, new RectangleF(4, 4, (float)75, (float)45), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+                    else
+                    {
+                        e.Graphics.DrawString("G: " + (selectedRow.Cells["gross"].Value ?? "0").ToString(), new Font("Arial", (float)9.5, FontStyle.Bold), brush, new RectangleF(4, 4, 75, (float)22.5), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+
+                        e.Graphics.DrawString("N: " + (selectedRow.Cells["net"].Value ?? "0").ToString(), new Font("Arial", (float)9.5, FontStyle.Bold), brush, new RectangleF(4, (float)26.5, 75, (float)22.5), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+
+                    }
+
+                    //logo
+                    Image logoImage = Image.FromFile(helper.ImageFolder + "\\logo.jpg"); // Replace with the actual path
+                    e.Graphics.DrawImage(logoImage, new RectangleF(83, 4, (float)22.5, (float)22.5));
+
+                    //logo name 
+
+                    if (selectedRow.Cells["type"].Value.ToString() == "BANGAL")
+                    {
+                        e.Graphics.DrawString((selectedRow.Cells[10].Value ?? "0").ToString(), new Font("Arial", (float)6, FontStyle.Bold), brush, new RectangleF(79, (float)28, (float)30.5, (float)11.25), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+                    else
+                    {
+
+                        e.Graphics.DrawString("YAMUNA", new Font("Arial", (float)4.5, FontStyle.Bold), brush, new RectangleF(79, (float)26.5, (float)30.5, (float)11.25), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+
+                    //caret
+                    e.Graphics.DrawString((selectedRow.Cells["caret"].Value ?? "0").ToString().Split('-')[0].Trim() ?? "0", new Font("Arial", (float)6, FontStyle.Bold), brush, new RectangleF(79, (float)37.75, (float)30.5, (float)11.25), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+
+                    // Draw the QR code rectangle
+                    RectangleF qrCodeRect = new RectangleF(174, 2, 37, 37);
+                    //  e.Graphics.DrawRectangle(Pens.Red, qrCodeRect.X, qrCodeRect.Y, qrCodeRect.Width, qrCodeRect.Height);
+                    using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+                    {
+                        QRCodeData qrCodeData = qrGenerator.CreateQrCode(tagNumber, QRCodeGenerator.ECCLevel.Q);
+                        QRCode qrCode = new QRCode(qrCodeData);
+                        System.Drawing.Bitmap qrCodeBitmap = qrCode.GetGraphic((int)qrCodeRect.Width, System.Drawing.Color.Black, System.Drawing.Color.White, true);
+                        // Draw the QR code onto the printing surface
+                        e.Graphics.DrawImage(qrCodeBitmap, qrCodeRect);
+                    }
+
+                    //labour                
+                    string labour = "0";
+                    if ((selectedRow.Cells["labour"].Value ?? "-").ToString() != "0")
+                    {
+                        labour = (selectedRow.Cells["labour"].Value ?? "-").ToString();
+                        e.Graphics.DrawString("L: " + labour, new Font("Arial", (float)7, FontStyle.Bold), brush, new RectangleF((float)113.5, (float)4, (float)56.5, (float)11), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+                    else if ((selectedRow.Cells["wholeLabour"].Value ?? "-").ToString() != "0")
+                    {
+                        labour = (selectedRow.Cells["wholeLabour"].Value ?? "-").ToString();
+                        e.Graphics.DrawString("TL: " + labour, new Font("Arial", (float)7, FontStyle.Bold), brush, new RectangleF((float)113.5, (float)4, (float)56.5, (float)11), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+
+                    //other
+                    if ((selectedRow.Cells["other"].Value ?? "0").ToString() != "0")
+                    {
+
+                        e.Graphics.DrawString("O: " + (selectedRow.Cells["other"].Value ?? "0").ToString(), new Font("Arial", (float)7, FontStyle.Bold), brush, new RectangleF((float)113.5, (float)16, (float)56.5, (float)11), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+
+                    //Tag number
+                    string firstPart = null;
+                    string secondPart = null;
+                    int length = tagNumber.Length;
+                    if (length >= 10)
+                    {
+                        int lastIndex = length - 5;
+                        firstPart = tagNumber.Substring(lastIndex);
+                        secondPart = tagNumber.Substring(0, lastIndex);
+                        e.Graphics.DrawString(secondPart, new Font("Arial", (float)6, FontStyle.Bold), brush, new RectangleF((float)113.5, (float)29, (float)56.5, (float)10), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                        e.Graphics.DrawString(firstPart, new Font("Arial", (float)6, FontStyle.Bold), brush, new RectangleF((float)113.5, (float)38, (float)56.5, (float)12), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+                    else
+                    {
+                        e.Graphics.DrawString(tagNumber, new Font("Arial", (float)7, FontStyle.Bold), brush, new RectangleF((float)113.5, (float)30, (float)56.5, (float)11), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+
+                    //huid
+                    //   e.Graphics.DrawRectangle(Pens.Red, (float)174, (float)37, (float)37, (float)5);
+                    //  e.Graphics.DrawRectangle(Pens.Red, (float)174, (float)43, (float)37, (float)5);
+
+                    string huid1 = (selectedRow.Cells["huid1"].Value ?? "0").ToString();
+                    string huid2 = (selectedRow.Cells["huid2"].Value ?? "0").ToString();
+                    if (huid1.Length == 6)
+                    {
+                        e.Graphics.DrawString(huid1, new Font("Arial", (float)5, FontStyle.Bold), brush, new RectangleF((float)174, (float)38, (float)37, (float)7), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+                    if (huid2.Length == 6)
+                    {
+                        e.Graphics.DrawString(huid2, new Font("Arial", (float)5, FontStyle.Bold), brush, new RectangleF((float)174, (float)44, (float)37, (float)7), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+                    }
+
+                    string updateQuery = $"UPDATE MAIN_DATA SET PRINT = '1'   WHERE TAG_NO = '{tagNumber}'";
+
+                    helper.RunQueryWithoutParametersSYADataBase(updateQuery, "ExecuteNonQuery");
+
+                }
+            }
+        }
+    }
+}
